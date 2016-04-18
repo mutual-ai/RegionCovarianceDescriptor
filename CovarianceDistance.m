a84b880487f20c60142fc8d49d96b06b9733cffd
nkosig <208504751@stu.ukzn.ac.za>
2016-04-19 00:17:41 +0200

    Eps to distance calc + Covariance not positive
    Adding eps to distance calculation - doesn't work
    Covariance matrix is still not definite positive

--- CovarianceDistance.m
+++ CovarianceDistance.m
@@ -19,14 +19,19 @@ function d = CovarianceDistance(C1,C2)
 global C
 d = 0;
 
-e = exp(1); 
+eps = 1e-6; 
 
 try
-  [~,D] = eig(C1,C2); %Calculating the generalised eigenvalues
+  [~,D] = eig(C1 + eps,C2 + eps); %Calculating the generalised eigenvalues
 catch
   C = C2;
   disp(C2);
   [~,D] = eigs(C1,C2); %Calculating the generalised eigenvalues
+end
+
+if(min(diag(D)) < -eps)
+    disp('error occurred in generalized eigenvalue computation');
+    return; 
 end
 
 d = sqrt(sum(log(diag(abs(D))).^2)); %Distance calculation
--- FeatureImage.m
+++ FeatureImage.m
@@ -7,51 +7,55 @@
 %  by equation 13
 %  Region Covariance: A Fast Descriptor for Detection and Classification
 %
-%  F(x,y) = [x y R(x,y) G(x,y) B(x,y) |dI/dx| |dI/dy| |d2I/dx2| |d2I/dy2|]
+%  F(x,y) = [x y R(x,y) G(x,y) B(x,y) |dI/dx| |dI/dy| |d^2I/dx^2| |d^2I/dy^2|]
 %
+%  F - Output feature image (W x H x d)
+%  RGB - 3 dimensional RGB color image (W x H x 3)
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%  Author
 %  Nkosikhona Gumede
 %  University of KwaZulu Natal
 %  208504751@stu.ukzn.ac.za
 %  Aug 2015
-%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function [F, I] = FeatureImage(RGB)
+
+function F = FeatureImage(RGB)
 
 % Convert to grayscale (intensity) image
 r = RGB(:, :, 1);
 g = RGB(:, :, 2);
 b = RGB(:, :, 3);
 
-I = rgb2gray(RGB); % Convert to grayscale (intensity) image 
+I = rgb2gray(RGB);      % Convert to grayscale (intensity) image 
 %I = ((r + g + b)/3);
-I = im2double(I);
+I = im2double(I);       %double intensity image I
 
 %I = squeeze(I(:,:,3));
 
 [h,w,~] = size(RGB);
 
-F = zeros(h,w,9);     % Feature image template  (H x W x d matrix)
+F = zeros(h,w,9);       % Feature image template  (H x W x d matrix)
 
 for i=1:h
     for j=1:w
-        F(i,:,1) = i*ones(1,w);   % Pixel x location
-        F(:,j,2) = j*ones(h,1);   % Pixel y location
+        F(i,:,1) = i*ones(1,w);         % Pixel x location
+        F(:,j,2) = j*ones(h,1);         % Pixel y location
     end
 end
 
-F(:,:,3:5) = im2double(RGB);    % RGB pixel values
+F(:,:,3:5) = im2double(RGB);            % double RGB pixel values
 
-xdev1 = [-1 0 1]';   % first derivate calculation filter
-ydev1 = xdev1';
+xdev1 = [-1 0 1]';      % first derivate calculation kernel (dx)
+ydev1 = xdev1';         % transpose for dy    
 
-xdev2 = [-1 2 -1]';  % second derivate calculation filter
-ydev2 = xdev2';
+xdev2 = [-1 2 -1]';     % second derivate calculation filter (dx^2)
+ydev2 = xdev2';         % transpose for dy^2
 
 F(:,:,6) = abs(conv2(I,xdev1,'same'));    % Absolute of first derivative w.r.t x |dI/dx|
 F(:,:,7) = abs(conv2(I,ydev1,'same'));    % Absolute of first derivative w.r.t y |dI/dy|
-F(:,:,8) = abs(conv2(I,xdev2,'same'));    % Absolute of second derivative w.r.t x |d2I/dx2|
-F(:,:,9) = abs(conv2(I,ydev2,'same'));    % Absolute of second derivative w.r.t y |d2I/dy2|
+F(:,:,8) = abs(conv2(I,xdev2,'same'));    % Absolute of second derivative w.r.t x |d^2I/dx^2|
+F(:,:,9) = abs(conv2(I,ydev2,'same'));    % Absolute of second derivative w.r.t y |d^2I/dy^2|
 
 %I = im2uint8(I);
 
